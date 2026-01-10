652c062409823fc12153ff2e6610dcef8dffd6f3
D0mano <njfotso@gmail.com>
2026-01-10 15:56:21 +0100

    Initial commit Creation of different class , GamePanel to manage the screen and the game loop, KeyHandler to manage the different keyboard inputs, Entity this will be the parent class of all future character added to the game and Player to manage the player info

---
+++ src/main/GamePanel.java
@@ -0,0 +1,103 @@
+package main;
+
+import entity.Player;
+
+import javax.swing.JPanel;
+import java.awt.*;
+
+public class GamePanel extends JPanel implements Runnable {
+
+
+    // SCREEN SETTINGS
+    final int originalTileSize = 16;        //16x16 Tiles
+    final int scale = 3;
+
+    public final int tileSize = originalTileSize * scale;   // 48x48 Tiles
+    final int maxScreenCol = 16;
+    final int maxScreenRow = 12;
+    final int screenWidth = tileSize * maxScreenCol;    // 768 pixels
+    final int screenHeight = tileSize * maxScreenRow;    // 576 pixels
+
+    //FPS
+    int FPS = 60;
+
+    KeyHandler keyH = new KeyHandler();
+    Thread gameThread;
+    Player player = new Player(this,this.keyH);
+
+
+
+
+
+
+    public GamePanel() {
+        this.setPreferredSize(new Dimension(screenWidth, screenHeight));
+        this.setBackground(Color.black);
+        this.setDoubleBuffered(true);
+        this.addKeyListener(keyH);
+        this.setFocusable(true);
+
+    }
+
+    public void startGameThread() {
+        gameThread = new Thread(this);
+        gameThread.start();
+    }
+
+    @Override
+    public void run() {
+
+        double drawInterval = (double) 1000000000 / FPS; // 0.016666 seconds
+        double delta = 0;
+        long lastTime = System.nanoTime();
+        long currentTime;
+        long timer = 0;
+        int drawCounter = 0;
+
+        while(gameThread != null){
+
+            currentTime = System.nanoTime();
+            delta += (currentTime - lastTime) / drawInterval;
+            timer += (currentTime - lastTime);
+            lastTime = currentTime;
+
+            if(delta >= 1){
+                // 1 UPDATE : update information such as character position
+                update();
+
+                //2 DRAW : draw the screen with the new update information
+                repaint();
+                delta--;
+                drawCounter++;
+
+            }
+            // Display FPS
+            if (timer >= 1000000000) {
+                System.out.println("FPS :" +drawCounter);
+                drawCounter = 0;
+                timer = 0;
+            }
+
+
+
+
+
+        }
+
+    }
+
+    public void update(){
+        player.update();
+
+
+    }
+    public void paintComponent(Graphics g) {
+
+        super.paintComponent(g);
+
+        Graphics2D g2d = (Graphics2D) g;
+        player.draw(g2d);
+        g2d.dispose();
+    }
+
+}
